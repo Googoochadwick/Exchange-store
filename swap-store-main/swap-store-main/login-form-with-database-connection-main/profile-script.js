8f57eadf67fc674a4458c7897d3bc905f5f9f5eb
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-01-18 19:27:07 +0530

    Delete swap-store-main directory

--- swap-store-main/swap-store-main/login-form-with-database-connection-main/profile-script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const editProfileBtn = document.getElementById('editProfileBtn');
-    
-    editProfileBtn.addEventListener('click', function() {
-       
-        alert('Edit profile functionality would be implemented here.');
-    });
-});
-
